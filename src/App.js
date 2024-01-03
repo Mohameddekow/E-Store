c27c6055ad7e3a9b120b168e67f0910fa7b537fb
mohameddekow <mohameddekow77@gmail.com>
2024-01-03 12:53:08 +0300

    install sass and create Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import CategoryItem from "./components/category-item/category-item.component";
+
+import "./categories.styles.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div className="App">
-      <h1>Hello MOha</h1>
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 };
