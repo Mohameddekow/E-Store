da38151ba01a5b19bea8fbcd15f2d1e503bca48a
mohameddekow <mohameddekow77@gmail.com>
2024-01-04 14:44:32 +0300

    routes and navigation components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+// import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
+    // <BrowserRouter>
     <Routes>
-      <Route path="/" index element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
+    // </BrowserRouter>
   );
 };
 
